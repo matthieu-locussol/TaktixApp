63b011b7d9f045b4891df442b46305542952fb31
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2022-05-12 13:48:27 +0200

    refactor: removed BottomPanel

--- src/ui/Game.tsx
+++ src/ui/Game.tsx
@@ -1,9 +1,7 @@
 import { Box } from '@mui/material';
-import { BottomPanel } from './layout/BottomPanel/BottomPanel';
 
 export const Game = () => (
    <Box>
       <Box id="phaser-container" />
-      <BottomPanel />
    </Box>
 );
