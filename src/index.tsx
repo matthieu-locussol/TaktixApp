b6e232924c63eba03f46cdda99e57a2e19389799
Matthieu LOCUSSOL <dev.matthieu.locussol@gmail.com>
2022-05-15 15:15:18 +0200

    refactor: moved theme/ under ui/ folder

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './game/PhaserGame';
 import { StoreProvider } from './store';
-import { theme } from './theme';
 import { Game } from './ui/Game';
 import { Layout } from './ui/layout/Layout';
+import { theme } from './ui/theme';
 
 ReactDOM.render(
    <React.StrictMode>
